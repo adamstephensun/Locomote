27fe77215a0d923273b04320f496418b8a0f299a
adamstephensun <adamski.david1@gmail.com>
2021-06-09 15:21:57 +0100

    Music and lighting added

---
+++ Locomote/Assets/Scripts/StartMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusic : MonoBehaviour
+{
+    private AudioSource audio;
+    private bool isPlaying;
+
+    void Start()
+    {
+        audio = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+        isPlaying = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !isPlaying)
+        {
+            Debug.Log("Start music" + isPlaying);
+            audio.Play();
+            isPlaying = true;
+        }
+    }
+}
